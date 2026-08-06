792fbd8f57316c42dcd197b0b7dc3ce5e2a9da78
elhamod <55500282+elhamod@users.noreply.github.com>
2026-08-06 12:08:14 -0400

    Update streamlit_app.py
    simple button and Gemini api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+from google import genai
+from google.genai import types
+from google.colab import userdata
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+### Load your API Key
+client = genai.Client(api_key=st.secrets['MyGeminiKey'])
+
+MODEL = "gemini-3.1-flash-lite"
+
+
+# st.title("🎈 My new app")
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
+st.write("Press the button to say hello")
+if st.button("Press me!"):
+    st.write("client.models.generate_content(model=MODEL, contents="Say hello in five words.").text")
+
